0ef18f108f28f3c7dff19dbe320e35a927251c10
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-01-29 23:37:54 +0530

    solved one another simple program of python

---
+++ LearningPython/LearningPython/NumbersAndOperators.py
@@ -0,0 +1,10 @@
+multiplication = 5 * 10
+print("Result of multiplication is :", multiplication)
+
+
+divisionInt = 122 / 23  #Weird result, need to ask someone
+divisionFloat = 122.0 / 23
+
+print("Integer division (122 / 23): ", divisionInt)  #Unexpected output
+print("Float division (122.0 / 23): ", divisionFloat)
+
